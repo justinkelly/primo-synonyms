25d3b7a4f62fad73dcbca977d4a24c89576c1e2d
Justin Kelly <justin@kelly.org.au>
2012-05-21 13:16:25 +1000

    readme cleanup

--- merge_primo_synonyms.pl
+++ merge_primo_synonyms.pl
@@ -17,36 +17,27 @@ sub trim($)
 	return $string;
 }
 
-print "What version of Primo are you using? :: ";
-$primo_version = <>;
-$primo_version = trim($primo_version);
-
 print "Do you want to include course codes? (y/n) :: ";
 $course_codes = <>;
 $course_codes = trim($course_codes);
 
 
-if($course_codes =~ m/^y/)
-{ 
-	print "What is the name of your course codes file ie. course_codes.txt? :: ";
-	$course_codes_file = <>;
-	$course_codes_file = trim($course_codes_file);
-}
-
-
-print "Please enter a file name for the new synonyms file, default is userSynonyms :: ";
-$output_file = <>;
-$output_file = trim($output_file);
-if($output_file eq '')
+print "Please enter a file name of your custom synonyms file, default is english_userSynonyms.txt";
+$custom_file = <>;
+$custom_file = trim($custom_file);
+if($customer_file eq '')
 {
-    $output_file = 'userSynonyms';
+    $custom_file = 'english_userSynonyms.txt';
 }
+
+$output_file = 'userSynonyms';
+
 
 #################################################
 #Merge the contents of the custom misspell file #
 #################################################
-open (CUSTOM, "custom_synonyms.txt");
-open (OUTPUT_CUSTOM, '>',"$primo_version/$output_file");
+open (CUSTOM, "$custom_file");
+open (OUTPUT_CUSTOM, '>',"$output_file");
 
 while (<CUSTOM>) {
 	chomp;
@@ -61,13 +52,13 @@ close(OUTPUT_CUSTOM);
 # Sort the new misspell file #
 ##############################
 
-open (NAMES_FILE, '<', "$primo_version/$output_file")  or  die "Failed to read file : $! ";
+open (NAMES_FILE, '<', "$output_file")  or  die "Failed to read file : $! ";
 my @not_sorted = <NAMES_FILE>;  # read entire file in the array
 close (NAMES_FILE);
 @sorted = sort { lc($a) cmp lc($b) } @not_sorted ;
 
 
-open(FILE, '>',"$primo_version/$output_file" ) || die "unable to open file write <$!>";
+open(FILE, '>',"$output_file" ) || die "unable to open file write <$!>";
   
   foreach(@sorted) {
     print FILE "$_";
@@ -75,4 +66,4 @@ open(FILE, '>',"$primo_version/$output_file" ) || die "unable to open file write
   
 close(FILE);
 
-print "Your new synonyms file has been saved to $primo_version/$output_file"
+print "Your new synonyms file has been saved to $output_file"
